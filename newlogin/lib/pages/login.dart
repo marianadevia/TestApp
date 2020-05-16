31e65de0237f3d5e11040b6e19b342049393385f
marianadevia <mldevia@gmail.com>
2020-05-16 19:14:05 -0400

    Login avanzando

--- newlogin/lib/pages/login.dart
+++ newlogin/lib/pages/login.dart
@@ -20,15 +20,70 @@ class _LoginPageState extends State<LoginPage> {
        height: size.height,
        child: Stack(
          children:<Widget>[
-           Positioned(
-             left: -100,
-             top: -100,
+            Positioned(
+             right: -size.width*0.25,
+             top: -size.width*0.36,
              child: Circle(
-               radius:200,
+               radius:size.width*0.45,
+               colors:[
+                 Colors.pink,
+                 Colors.pinkAccent
+               ]
+             )
+           ),
+
+           Positioned(
+             left: -size.width*0.15,
+             top: -size.width*0.34,
+             child: Circle(
+               radius:size.width*0.35,
                colors:[
                  Colors.orange,
                  Colors.deepOrange
                ]
+             )
+           ),
+           SingleChildScrollView(
+             child: SafeArea(
+                            child: Container(
+                 width: size.width,
+                 
+                 child: Column(
+                   children:<Widget>[
+                     Column(
+                       children: <Widget>[
+                         Container(
+                           width:90,
+                           height:90,
+                           margin: EdgeInsets.only(top:size.width*0.3),
+                           decoration:BoxDecoration(
+                             color: Colors.white,
+                             borderRadius: BorderRadius.circular(15),
+                             boxShadow:[
+                               BoxShadow(
+                                 color:Colors.black26,
+                                 blurRadius:25
+                               )
+                             ]
+                           )
+                         ),
+                         SizedBox(height: 30,),
+                         Text(
+                           'Hello again.\ Welcome Back', 
+                            textAlign: TextAlign.center, 
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w100,
+
+                            )
+                          )
+                       ],
+                     ),
+
+                     Column(),
+                   ]
+                 ),
+               ),
              )
            )
          ]
